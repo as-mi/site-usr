b8e116ab860a5fc84511b2d996d10b738aa5802d
Gabriel Majeri <gabriel.majeri6@gmail.com>
2020-07-02 16:25:26 +0300

    Obține dinamic conținutul paginii

--- usr/header.php
+++ usr/header.php
@@ -11,15 +11,25 @@
   <?php body_class(); ?>
 >
 
-<nav id="top-bar" class="navbar position-sticky">
-  <?php
-  the_custom_logo();
-  ?>
+<nav class="navbar navbar-expand-xl navbar-dark">
+  <div class="container">
+    <a class="navbar-brand" href="/">
+    USR
+    <?php
+    the_custom_logo();
+    ?>
+    </a>
+    <button class="navbar-toggler bg-dark" type="button" data-toggle="collapse" data-target="#navCollapse" aria-controls="navCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <?php
-  wp_nav_menu(array(
-      'theme_location' => 'primary-menu',
-      'menu_class' => 'nav',
-  ));
-  ?>
+    <?php
+    wp_nav_menu(array(
+        'theme_location' => 'primary-menu',
+        'menu_class' => 'navbar-nav',
+        'container_class' => 'collapse navbar-collapse',
+        'container_id' => 'navCollapse',
+    ));
+    ?>
+  </div>
 </nav>
--- usr/index.php
+++ usr/index.php
@@ -2,19 +2,16 @@
 get_header();
 ?>
 
-<main>
-    <h1>Uniunea Studenților din România</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit excepturi totam facilis autem. Minima nesciunt, at aperiam libero labore modi laboriosam veniam dicta eveniet quisquam non rerum vero iure sint.
-    </p>
-
-    <style>
-      @media (min-width: 50em) {
-        h1 {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
+<main class="container">
+  <h1><?php the_title(); ?></h1>
+  <?php
+  if (have_posts()) {
+    while (have_posts()) {
+      the_post();
+      the_content();
+    }
+  }
+  ?>
 </main>
 
 <?php
