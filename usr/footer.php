0a24ee7d631240fe6912b6b59a71741fcdef59cc
Gabriel Majeri <gabriel.majeri6@gmail.com>
2020-07-02 15:50:11 +0300

    Mută iconițele de social media în `footer.php`

--- usr/footer.php
+++ usr/footer.php
@@ -1,3 +1,28 @@
+<aside id="social-media-icons">
+  <div class="float-left m-3">
+    <a href="https://www.instagram.com/studentie.usr/" target="_blank">
+      <img alt="Instagram" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/instagram.png"/>
+    </a>
+    <a href="https://www.facebook.com/UniuneaStudentilordinRomania/" target="_blank">
+      <img alt="Facebook" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/facebook.png"/>
+    </a>
+  </div>
+  <div class="float-right m-3">
+    <a href="https://www.youtube.com/channel/UCUC5P2GzChEzQyPL1K8hQQg" target="_blank">
+      <img alt="YouTube" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/youtube.png"/>
+    </a>
+    <a href="https://www.linkedin.com/company/uniunea-studen-ilor-din-rom-nia/" target="_blank">
+      <img alt="LinkedIn" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/linkedin.png"/>
+    </a>
+  </div>
+</aside>
+
+<script>
+// Enable VS Code Live Reload
+document.write('<script src="http://localhost:35729/livereload.js?snipver=1">'
+  + '</' + 'script>');
+</script>
+
 <?php
 wp_footer();
 ?>
