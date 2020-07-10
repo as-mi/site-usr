e4d784b0ec070e026f9f94ea67501d2597ac1d52
Andrei Micuda <andrei.micuda@gmail.com>
2020-07-10 22:07:58 +0300

    Finalizat header si footer (incl. mobile responsive) (#12)

--- usr/header.php
+++ usr/header.php
@@ -1,35 +1,47 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-  <?php
-  wp_head();
-  ?>
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- <title>USR - Uniunea Studentilor din Romania</title> -->
+  <?php wp_head(); ?>
+  
 </head>
 
-<body
-  <?php body_class(); ?>
->
+<body>
+  <header class="pure-menu pure-menu-horizontal">
+    <svg class="header-menu-btn hidden" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="#020202">
+        <path d="M3 12h18"></path>
+        <path d="M3 6h18"></path>
+        <path d="M3 18h18"></path>
+      </g>
+    </svg>
 
-<nav class="navbar navbar-expand-xl navbar-dark">
-  <div class="container">
-    <a class="navbar-brand" href="/">
-    USR
-    <?php
-    the_custom_logo();
-    ?>
-    </a>
-    <button class="navbar-toggler bg-dark" type="button" data-toggle="collapse" data-target="#navCollapse" aria-controls="navCollapse" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <?php
-    wp_nav_menu(array(
-        'theme_location' => 'primary-menu',
-        'menu_class' => 'navbar-nav',
-        'container_class' => 'collapse navbar-collapse',
-        'container_id' => 'navCollapse',
-    ));
-    ?>
-  </div>
-</nav>
+    <img class="header-logo" src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="Logo USR">
+    <ul class="header-nav pure-menu-list">
+      <li class="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
+        <a href="despre.html" class="pure-menu-link">Despre</a>
+        <ul class="pure-menu-children">
+          <li class="pure-menu-item"><a href="cons_dir.html" class="pure-menu-link">Consiliu Director</a></li>
+          <li class="pure-menu-item"><a href="structura.html" class="pure-menu-link">Structura</a></li>
+          <li class="pure-menu-item"><a href="statut.html" class="pure-menu-link">Statut</a></li>
+        </ul>
+      </li>
+      <li class="pure-menu-item"><a href="proiecte.html" class="pure-menu-link">Proiecte</a></li>
+      <li class="pure-menu-item"><a href="membri.html" class="pure-menu-link">Membri</a></li>
+      <li class="pure-menu-item"><a href="training.html" class="pure-menu-link">Training-uri</a></li>
+      <li class="pure-menu-item"><a href="news.html" class="pure-menu-link">News</a></li>
+      <li class="pure-menu-item"><a href="comunicate.html" class="pure-menu-link">Comunicate</a></li>
+      <li class="pure-menu-item"><a href="parteneri.html" class="pure-menu-link">Parteneri</a></li>
+      <li class="pure-menu-item"><a href="contact.html" class="pure-menu-link">Contact</a></li>
+    </ul>
+    <div class="header-divider">
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path
+          d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+          class="shape-fill"></path>
+      </svg>
+    </div>
+  </header>
