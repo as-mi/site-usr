7968e9893d77f63d555865215d1906fa7a898716
Andrei Micuda <andrei.micuda@gmail.com>
2020-08-08 11:14:04 +0300

    Adaugat carusel postari recente pe pagina de start (#15)

--- usr/index.php
+++ usr/index.php
@@ -9,6 +9,32 @@
       est. Praesentium quis, blanditiis voluptatibus repellendus maiores reiciendis nulla ab.
     </p>
 
+    <div class="carousel-wrapper">
+      <div class="carousel"
+        data-flickity='{
+          "cellAlign": "left",
+          "pageDots": false,
+          "wrapAround": true
+        }'>
+        <!-- <div class="carousel-cell">1</div> -->
+        <?php
+          $recent_posts = wp_get_recent_posts(array(
+            'numberposts' => 4, // Number of recent posts thumbnails to display
+            'post_status' => 'publish' // Show only the published posts
+          ));
+          foreach($recent_posts as $post) : ?>
+            <div class="carousel-cell">
+              <a href="<?php echo get_permalink($post['ID']) ?>">
+                <?php echo get_the_post_thumbnail($post['ID'], 'full'); ?>
+                <figcaption><?php echo $post['post_title'] ?></figcaption>
+              </a>
+            </div>
+        <?php endforeach; wp_reset_query(); ?>
+      </div>
+    </div>
+
   </main>
+
+  <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
 
 <?php get_footer() ?>
