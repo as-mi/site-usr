e5446325f4419193629f2a5367169cd1f7430a90
Gabriel Majeri <gabriel.majeri6@gmail.com>
2020-09-25 16:52:16 +0300

    Adaugă pagină custom pentru 404

---
+++ usr/404.php
@@ -0,0 +1,8 @@
+<?php get_header() ?>
+
+<main class="textbox">
+    <h2>Pagină negăsită</h2>
+    <p>Nu am putut găsi pagina căutată. Vă rugăm să verificați din nou linkul.</p>
+</main>
+
+<?php get_footer() ?>
